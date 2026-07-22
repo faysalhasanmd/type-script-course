dcc699f9ba2a9352135d3aa0dca455cff9570f63
Md.Faysal Hasan <faysalhasanmd393@mail.com>
2026-07-22 11:22:24 +0600

    solve tuple type

---
+++ user-defind-type/tuple-type.js
@@ -0,0 +1,5 @@
+"use strict";
+let people;
+people = [1223, "faysal"];
+people.push("hasan", 321);
+console.log(people, people[1]);
---
+++ user-defind-type/tuple-type.ts
@@ -0,0 +1,6 @@
+let people: [number, string];
+people = [1223, "faysal"];
+
+people.push("hasan", 321);
+
+console.log(people, people[1]);
