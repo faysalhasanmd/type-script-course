8fa2449b734ecbd67e0958117a73d347b434cc57
Md.Faysal Hasan <faysalhasanmd393@mail.com>
2026-07-22 11:04:36 +0600

    solve array type

---
+++ user-defind-type/arry-type.js
@@ -0,0 +1,6 @@
+"use strict";
+let arrayType;
+arrayType = [1, 2, 1, 5, 5];
+let multipleType;
+multipleType = [1, "faysal", true];
+console.log(arrayType, multipleType, multipleType[1]);
---
+++ user-defind-type/arry-type.ts
@@ -0,0 +1,7 @@
+let arrayType: number[];
+arrayType = [1, 2, 1, 5, 5];
+
+let multipleType: (number | string | boolean)[];
+multipleType = [1, "faysal", true];
+
+console.log(arrayType, multipleType, multipleType[1]);
